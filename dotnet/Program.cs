227bd73187d0a372b942bc97d9443d81391ef47f
Kakadu <Kakadu@pm.me>
2023-03-16 00:24:09 +0300

    Add dotnet partial benchmark
    Signed-off-by: Kakadu <Kakadu@pm.me>

---
+++ dotnet/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+[MemoryDiagnoser]
+public class Program
+{
+/*
+    int n = 10_000;
+
+    [Benchmark]
+    public string Builder()
+    {
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < n; i++)
+        {
+            output.Append("falcon").Append(i);
+        }
+
+        return output.ToString();
+    }
+
+    [Benchmark]
+    public string Interpolation()
+    {
+        string output = string.Empty;
+
+        for (int i = 0; i < n; i++)
+        {
+            output = $"{output}falcon{i}";
+        }
+
+        return output;
+    }
+
+    [Benchmark]
+    public string Addition()
+    {
+        string output = string.Empty;
+
+        for (int i = 0; i < n; i++)
+        {
+            output += "falcon" + i;
+        }
+
+        return output.ToString();
+    }
+*/
+    Shapes[] array = null;
+    uint ShapesCount = 100;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        array = new Shapes[ShapesCount];
+        for (var i=0; i<ShapesCount; i++) {
+          switch (i%4) {
+            case 0: array[i] = new Rectangle(5.0f, 4.0f); break;
+            case 1: array[i] = new Triangle(5.0f, 4.0f); break;
+            case 2: array[i] = new Square(5.0f); break;
+            default: array[i] = new Circle(5.0f);
+              break;
+          }
+        }
+    }
+    [Benchmark]
+    public void TotalAreaVTBL()
+    {
+        ShapesBench.TotalAreaVTBL(ShapesCount, array);
+    }
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Program>();
+        //var x = new Shapes();
+    }
+}
---
+++ dotnet/Shapes.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+abstract class Shapes {
+  public float Width  { get; set; }
+  public float Height { get; set; }
+  public abstract float Area();
+  public abstract uint CornerCount();
+}
+
+class Square : Shapes {
+  public Square(float Width) {
+    this.Width = this.Height = Width;
+  }
+  public override float Area() { return Width * Width; }
+  public override uint CornerCount() { return 4; }
+}
+
+class Rectangle : Shapes {
+  public Rectangle(float Width, float Height ) {
+    this.Width = Width;
+    this.Height = Height;
+  }
+  public override float Area()  { return Width * Height; }
+  public override uint CornerCount() { return 4; }
+}
+
+class Circle : Shapes {
+  public Circle(float Radius) {
+    this.Width = Radius;
+  }
+  public override float Area()  { return Width / 2 * (float)Math.PI; }
+  public override uint CornerCount() { return 0; }
+}
+
+class Triangle : Shapes {
+  public Triangle(float Width, float Height) {
+    this.Width = Width;
+    this.Height = Height;
+  }
+  public override float Area() { return Width * Height / 2; }
+  public override uint CornerCount() { return 3; }
+}
+
+class ShapesBench {
+  public static float TotalAreaVTBL(uint Count, Shapes[] arr) {
+    var acc = 0.0f;
+    foreach (var s in arr)
+      acc += s.Area();
+    return acc;
+  }
+  public static float CornerAreaVTBL(uint Count, Shapes[] arr) {
+    var acc = 0.0f;
+    foreach (var s in arr)
+      acc += (1.0f / (1.0f + s.CornerCount())) * s.Area();
+    return acc;
+  }
+
+}
